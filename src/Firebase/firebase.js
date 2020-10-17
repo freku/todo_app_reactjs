f042e6e629480e105c12afcfa9f6fdde2c63f003
freku <psfq0@hotmail.com>
2020-10-17 21:12:15 +0200

    removed junk

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA3AU0f35f4B0vKvpruxzYR2vNokwGdWZs",
+  authDomain: "todoreactjs-83232.firebaseapp.com",
+  databaseURL: "https://todoreactjs-83232.firebaseio.com",
+  projectId: "todoreactjs-83232",
+  storageBucket: "todoreactjs-83232.appspot.com",
+  messagingSenderId: "153497891629",
+  appId: "1:153497891629:web:5b8ff1d1daf42c2b8059a4",
+  measurementId: "G-MJWSXD3XG3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
