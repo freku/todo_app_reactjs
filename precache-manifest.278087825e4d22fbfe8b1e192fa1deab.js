4300497d288533462ca2142b4b19338e2c149937
freku <psfq0@hotmail.com>
2020-10-27 17:10:37 +0100

    Updates

---
+++ precache-manifest.278087825e4d22fbfe8b1e192fa1deab.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abbf06fb6a8ae415eeb12b7b7df54eb",
+    "url": "/todo_app_reactjs/index.html"
+  },
+  {
+    "revision": "2588269c9b9dc987f14b",
+    "url": "/todo_app_reactjs/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "07cc235785053bce76a9",
+    "url": "/todo_app_reactjs/static/css/main.d9c787fd.chunk.css"
+  },
+  {
+    "revision": "2588269c9b9dc987f14b",
+    "url": "/todo_app_reactjs/static/js/2.34b91ef5.chunk.js"
+  },
+  {
+    "revision": "07ccaa3a99a22dae037f3758d9f5955d",
+    "url": "/todo_app_reactjs/static/js/2.34b91ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cc235785053bce76a9",
+    "url": "/todo_app_reactjs/static/js/main.76420698.chunk.js"
+  },
+  {
+    "revision": "a90683de830ad4e6df68",
+    "url": "/todo_app_reactjs/static/js/runtime-main.03539738.js"
+  },
+  {
+    "revision": "5e54b59d88dc32f4d47ce61237a24c67",
+    "url": "/todo_app_reactjs/static/media/Rolling-1s-200px.5e54b59d.svg"
+  },
+  {
+    "revision": "eb8fbeeedd9afbb5797105fac8338a4c",
+    "url": "/todo_app_reactjs/static/media/add.eb8fbeee.svg"
+  },
+  {
+    "revision": "7948b0b947ee0af6ed93f249a838caf2",
+    "url": "/todo_app_reactjs/static/media/add_circle_outline-24px.7948b0b9.svg"
+  },
+  {
+    "revision": "40908ebd66a06f80297991dc23956efa",
+    "url": "/todo_app_reactjs/static/media/cancel-24px.40908ebd.svg"
+  },
+  {
+    "revision": "7d49ab5d06082f4b7324e5c211af7eed",
+    "url": "/todo_app_reactjs/static/media/circle-checked.7d49ab5d.svg"
+  },
+  {
+    "revision": "b840e171a500c69863f00f6b4056b486",
+    "url": "/todo_app_reactjs/static/media/clear-24px.b840e171.svg"
+  },
+  {
+    "revision": "c2a415e1972504eae2527c72d0d60d87",
+    "url": "/todo_app_reactjs/static/media/crop_din-24px.c2a415e1.svg"
+  },
+  {
+    "revision": "a24d83e3452a8bfeefadc83930409188",
+    "url": "/todo_app_reactjs/static/media/delete_outline-24px.a24d83e3.svg"
+  },
+  {
+    "revision": "d09950b267d4e77a65139a3642593210",
+    "url": "/todo_app_reactjs/static/media/event-24px.d09950b2.svg"
+  },
+  {
+    "revision": "4ba2843a18d4113eacf6d6b1530edbe1",
+    "url": "/todo_app_reactjs/static/media/exit_to_app-24px.4ba2843a.svg"
+  },
+  {
+    "revision": "f3b50208677e437d17ff70c41c8f76da",
+    "url": "/todo_app_reactjs/static/media/format_list_bulleted-24px.f3b50208.svg"
+  },
+  {
+    "revision": "8816162173b901b522b538efe0d6bf7c",
+    "url": "/todo_app_reactjs/static/media/google.88161621.svg"
+  },
+  {
+    "revision": "ecce737b94780a3d592a82d8070d87a8",
+    "url": "/todo_app_reactjs/static/media/grade-24px-full.ecce737b.svg"
+  },
+  {
+    "revision": "24388236f9b25ea7c4a8dcd5982079ba",
+    "url": "/todo_app_reactjs/static/media/grade-24px.24388236.svg"
+  },
+  {
+    "revision": "6ebccfdf371cd9981f466d1a0c9ebc81",
+    "url": "/todo_app_reactjs/static/media/list_alt-24px.6ebccfdf.svg"
+  },
+  {
+    "revision": "87f2b782cc0ae3c4420d877e8cca29ba",
+    "url": "/todo_app_reactjs/static/media/loading.87f2b782.svg"
+  },
+  {
+    "revision": "c6263b300023baa3087ab7af506481ef",
+    "url": "/todo_app_reactjs/static/media/menu-24px.c6263b30.svg"
+  },
+  {
+    "revision": "5ac9fc30daf76b72667a9688514ae31b",
+    "url": "/todo_app_reactjs/static/media/send-24px.5ac9fc30.svg"
+  },
+  {
+    "revision": "f55dba4930e32173e499e2eeafdab5eb",
+    "url": "/todo_app_reactjs/static/media/wb_sunny-24px.f55dba49.svg"
+  }
+]);
